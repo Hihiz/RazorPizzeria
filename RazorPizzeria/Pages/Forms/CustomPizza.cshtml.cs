00df52012120da7d47356f2665b8b0bb08115e87
Hihiz <public@mail.ru>
2023-02-26 23:14:20 +0300

    При выборе ингредиента увеличивать цену

--- RazorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
+++ RazorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
@@ -8,9 +8,26 @@ namespace RazorPizzeria.Pages.Forms
     {
         [BindProperty] /*привязка*/
         public PizzasModel Pizza { get; set; }
+        public float PizzaPrice { get; set; }
 
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            PizzaPrice = Pizza.BasePrice;
+
+            if (Pizza.TomatoSauce) PizzaPrice += 1;
+            if (Pizza.Cheese) PizzaPrice += 5;
+            if (Pizza.Peperoni) PizzaPrice += 2;
+            if (Pizza.Mushroom) PizzaPrice += 7;
+            if (Pizza.Tuna) PizzaPrice += 4;
+            if (Pizza.Peperoni) PizzaPrice += 20;
+            if (Pizza.Ham) PizzaPrice += 20;
+            if (Pizza.Beef) PizzaPrice += 2;
+
+            return RedirectToPage("/Checkout/Checkout", new { Pizza.PizzaName, PizzaPrice });
         }
     }
 }
